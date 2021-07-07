cf2cd0e278323090616b77ed07e6f81f821730d7
Lindsay <lindsay.chen@yale.edu>
2021-07-08 06:01:55 +0800

    commit tree 15f36e81949d22dbadc265c8df53e23f60569c44

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :school_classes, only: [:index, :show, :edit, :update, :new, :create]
-  resources :students, only: [:index, :show, :edit, :update, :new, :create]
+  resources :school_classes, :students, only: [:index, :show, :edit, :update, :new, :create]
+  # resources :students, only: [:index, :show, :edit, :update, :new, :create]
 end
